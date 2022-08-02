88dcbc8b7a5cf141dfe0a39f489959309bbba238
songmingyu <china_songmingyu@163.com>
2022-08-02 17:37:01 +0800

    提交

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,23 +1,33 @@
 <!--
  * @Description: 
  * @Date: 2022-07-31 19:08:07
- * @LastEditTime: 2022-07-31 21:36:12
+ * @LastEditTime: 2022-08-02 11:27:14
  * @FilePath: \app\src\pages\Home\index.vue
 -->
 <template>
   <div>
     <TypeNav></TypeNav>
     <ListContainer></ListContainer>
+    <Recommend></Recommend>
+    <Rank></Rank>
+    <Like></Like>
+    <Floor></Floor>
+    <Floor></Floor>
+    <Brand></Brand>
   </div>
 </template>
 
 <script>
-import ListContainer from "./ListContainer/index.vue";
+import ListContainer from "@/pages/Home/ListContainer/index.vue";
+import Recommend from "@/pages/Home/Recommend/index.vue";
+import Rank from "@/pages/Home/Rank/index.vue";
+import Like from "@/pages/Home/Like/index.vue";
+import Floor from "@/pages/Home/Floor/index.vue";
+import Brand from "@/pages/Home/Brand/index.vue";
 export default {
-    components: { ListContainer }
-}
+  components: { ListContainer, Recommend, Rank, Like, Floor, Brand },
+};
 </script>
 
 <style>
-
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,19 @@
 /*
  * @Description: 
  * @Date: 2022-07-31 18:29:16
- * @LastEditTime: 2022-07-31 18:36:11
+ * @LastEditTime: 2022-08-02 11:35:13
  * @FilePath: \app\vue.config.js
  */
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave:false, //关闭eslint
+  //配置代理跨域
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://gmall-h5-api.atguigu.cn",
+      },
+    },
+  },
 })
